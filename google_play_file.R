3b266b0ec4d2ca1fb319e2ce9e0b32e0597e734b
Joe Allen-Black <jmccleaveblack@gmail.com>
2025-10-04 09:10:12 -0400

    Update google_play_file.R
    Another update. Removed the GET function. Not sure why it stopped working

--- google_play_file.R
+++ google_play_file.R
@@ -26,7 +26,7 @@ sites <- tribble (~siteName, ~url,
                   'Kaiser Permanente', 'https://play.google.com/store/apps/details?id=org.kp.m',
                   'Primary Care On Demand Wis.', 'https://play.google.com/store/apps/details?id=org.mayoclinic.primarycareondemand'
                   
-                  )
+)
 
 iterations <- nrow(sites)
 strings <- stri_rand_strings(iterations, 30, pattern = "[A-Za-z0-9]")
@@ -36,37 +36,30 @@ strings <- stri_rand_strings(iterations, 30, pattern = "[A-Za-z0-9]")
 for(i in 1:iterations) {
   
   url <- sites$url[i]
-  res <- GET(url, user_agent("Mozilla/5.0"))
+  webpage <- read_html(url)
   
+  ratings <- webpage %>% html_elements(".wcB8se") %>% html_attr('title') 
   
-  if (status_code(res) == 200) {
-    webpage <- read_html(res)
-    ratings <- webpage %>% html_elements(".wcB8se") %>% html_attr('title')
-    ratings_5_star <- as.numeric(gsub(",", "", ratings[1])) 
-    ratings_4_star <- as.numeric(gsub(",", "", ratings[2])) 
-    ratings_3_star <- as.numeric(gsub(",", "", ratings[3]))
-    ratings_2_star <- as.numeric(gsub(",", "", ratings[4]))
-    ratings_1_star <- as.numeric(gsub(",", "", ratings[5]))
-    total <- sum(ratings_5_star, ratings_4_star, ratings_3_star, ratings_2_star, ratings_1_star)
-    
-    average <- sum((ratings_5_star *5) + (ratings_4_star *4) + (ratings_3_star *3) + (ratings_2_star *2) + (ratings_1_star))/total
-    
-    output <- output %>% add_row (
-      date = timestamp, 
-      site = sites$siteName[i], 
-      id = strings[i], 
-      five_star = ratings_5_star, 
-      four_star = ratings_4_star, 
-      three_star = ratings_3_star, 
-      two_star = ratings_2_star, 
-      one_star = ratings_1_star, 
-      total = total,
-      average = average)
-
-} else {
-  print(paste("Failed to fetch:", status_code(res), " ", sites$sitename[i]))
-}
+  ratings_5_star <- as.numeric(gsub(",", "", ratings[1])) 
+  ratings_4_star <- as.numeric(gsub(",", "", ratings[2])) 
+  ratings_3_star <- as.numeric(gsub(",", "", ratings[3]))
+  ratings_2_star <- as.numeric(gsub(",", "", ratings[4]))
+  ratings_1_star <- as.numeric(gsub(",", "", ratings[5]))
+  total <- sum(ratings_5_star, ratings_4_star, ratings_3_star, ratings_2_star, ratings_1_star)
+  
+  average <- sum((ratings_5_star *5) + (ratings_4_star *4) + (ratings_3_star *3) + (ratings_2_star *2) + (ratings_1_star))/total
+  
+  output <- output %>% add_row (
+    date = timestamp, 
+    site = sites$siteName[i], 
+    id = strings[i], 
+    five_star = ratings_5_star, 
+    four_star = ratings_4_star, 
+    three_star = ratings_3_star, 
+    two_star = ratings_2_star, 
+    one_star = ratings_1_star, 
+    total = total,
+    average = average)
 }
 
 write.table(output,paste0('android_ratings.csv'),append = TRUE, sep=',', col.names = FALSE)   
-
